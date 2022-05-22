9101750cc0f5f4db7d9db4b7c148140659e8777c
Natanxdg <105949591+Natanxdg@users.noreply.github.com>
2022-05-21 22:05:55 -0300

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,15 @@
+function emBreve() {
+document.getElementById('fm').innerHTML = 'Em breve';
+}
+
+function aDisponivel() {
+document.getElementById('sm').innerHTML = 'Disponível';
+}
+
+function cGratuitamente() {
+document.getElementById('a').innerHTML = 'Gratuitamente';
+}
+
+function dWait() {
+document.getElementById('n').innerHTML = 'Aguarde !';
+}
